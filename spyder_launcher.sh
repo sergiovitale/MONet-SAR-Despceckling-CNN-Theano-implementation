1832dfd8d28d1d8d7c90738b8abfb55ab1073175
Sergio Vitale <sergio.vitale@uniparthenope.it>
2020-10-26 18:44:20 +0100

    Delete spyder_launcher.sh

--- spyder_launcher.sh
+++
@@ -1,6 +0,0 @@
-export PATH=/usr/lib/nvidia-cuda-toolkit/bin:$PATH
-export LD_LIBRARY_PATH=/usr/lib/nvidia-cuda-toolkit/cuda/lib64:$LD_LIBRARY_PATH
-export CPATH=/usr/lib/nvidia-cuda-toolkit/cuda/include:$CPATH
-export LIBRARY_PATH=/usr/lib/nvidia-cuda-toolkit/cuda/lib64:$LIBRARY_PATH
-export THEANO_FLAGS='cuda.root=//usr/lib/nvidia-cuda-toolkit/bin,device=cuda0,floatX=float32,init_gpu_device=cuda0'
-spyder
